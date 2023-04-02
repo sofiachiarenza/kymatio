fba0404fddfb98bb68e5504ad3cde436ae923774
sofiachiarenza <86685122+sofiachiarenza@users.noreply.github.com>
2023-04-02 17:59:28 +0200

    try again

--- kymatio/scattering3d/backend/torch_backend.py
+++ kymatio/scattering3d/backend/torch_backend.py
@@ -146,6 +146,7 @@ class TorchBackend3D(TorchBackend):
         integrals = torch.zeros((input_array.shape[0], len(integral_powers)),
                 device=input_array.device)
         mask = mask.reshape(input_array.shape)
+        print("Mask shape from Kymatio: " , mask.shape)
         for i_q, q in enumerate(integral_powers):
                 integrals[:, i_q] = (input_array[mask] ** q).view(
                 input_array.shape[0], -1).sum(1)
